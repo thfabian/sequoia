8a4f2f506b28c8fb80f89c64ed6f7bedc403f799
Fabian <fabian_thuering@hotmail.com>
2017-11-01 11:31:49 +0100

    Add support for cereal

--- cmake/External_cereal.cmake
+++ cmake/External_cereal.cmake
@@ -13,23 +13,25 @@
 ##
 ##===------------------------------------------------------------------------------------------===##
 
-find_package(Protobuf 3.4 NO_MODULE REQUIRED)
-
-get_property(Protobuf_INCLUDE_DIRS 
-  TARGET protobuf::libprotobuf 
-  PROPERTY INTERFACE_INCLUDE_DIRECTORIES
+ExternalProject_Add(
+  cereal
+  DOWNLOAD_DIR "${SEQUOIA_EXTERNAL_DOWNLOAD_DIR}"
+  URL ${cereal_url}
+  URL_MD5 ${cereal_md5}
+  BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}/cereal"
+  INSTALL_DIR "${SEQUOIA_EXTERNAL_INSTALL_PREFIX}/cereal"
+  PATCH_COMMAND ""
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ""
+  INSTALL_COMMAND ${CMAKE_COMMAND} -E make_directory <INSTALL_DIR>/include && 
+                  ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/include <INSTALL_DIR>/include
 )
 
-get_property(Protobuf_PROTOC_EXECUTABLE 
-  TARGET protobuf::protoc 
-  PROPERTY LOCATION
-)
+ExternalProject_Get_Property(cereal install_dir)
+set(CEREAL_ROOT "${install_dir}" CACHE INTERNAL "")
 
 sequoia_export_package(
-  NAME Protobuf
-  FOUND ${Protobuf_FOUND}
-  EXECUTABLE "${Protobuf_PROTOC_EXECUTABLE}"
-  INCLUDE_DIRS "${Protobuf_INCLUDE_DIRS}"
-  LIBRARIES "protobuf::libprotobuf"
-  VERSION "${Protobuf_VERSION}"
+  PACKAGE cereal 
+  CMAKE_ARGS 
+    "-DCEREAL_ROOT:PATH=${CEREAL_ROOT}"
 )
---
+++ sequoia-cmake/cmake/Findcereal.cmake
@@ -0,0 +1,88 @@
+##===------------------------------------------------------------------------------*- CMake -*-===##
+##                         _____                        _
+##                        / ____|                      (_)
+##                       | (___   ___  __ _ _   _  ___  _  __ _
+##                        \___ \ / _ \/ _` | | | |/ _ \| |/ _` |
+##                        ____) |  __/ (_| | |_| | (_) | | (_| |
+##                       |_____/ \___|\__, |\__,_|\___/|_|\__,_| - Game Engine (2016-2017)
+##                                       | |
+##                                       |_|
+##
+## This file is distributed under the MIT License (MIT).
+## See LICENSE.txt for details.
+##
+##===------------------------------------------------------------------------------------------===##
+
+#.rst:
+# Findcereal
+# -------
+#
+# .. code-block:: cmake
+#
+#   find_package(cereal 
+#     [REQUIRED]             # Fail with error if cereal is not found
+#     [QUIET]                # Supress output
+#   )
+#
+# This module locates cereal_. ``CEREAL_FOUND`` will report if an acceptable version of cereal was 
+# found.
+#
+# Result Variables
+# ^^^^^^^^^^^^^^^^
+#
+# Defines the following variables:
+#
+# ``CEREAL_FOUND``
+#  System has the cereal headers.
+# ``CEREAL_INCLUDE_DIRS``
+#  The location of cereal headers i.e the header ``cereal.hpp`` is in 
+#  ``${CEREAL_INCLUDE_DIRS}/cereal/cereal.hpp``.
+#
+# Hints
+# ^^^^^
+#
+# Set ``CEREAL_ROOT`` to a directory that contains a cereal installation, or directly set 
+# ``CEREAL_INCLUDE_DIRS`` if the module has trouble finding cereal.
+#
+# .. _cereal: https://github.com/USCiLab/cereal
+#
+
+include(FindPackageHandleStandardArgs)
+
+#
+# Parse CEREAL_ROOT
+#
+set(CEREAL_ROOT_ENV $ENV{CEREAL_ROOT})
+if(CEREAL_ROOT_ENV)
+  set(CEREAL_ROOT ${CEREAL_ROOT_ENV} CACHE PATH "cereal install path.")
+endif()
+
+if(NOT(CEREAL_ROOT))
+  find_path(CEREAL_ROOT NAMES cereal/cereal.hpp HINTS ${CEREAL_INCLUDE_DIRS})
+else()
+  get_filename_component(_CEREAL_ROOT_ABSOLUTE ${CEREAL_ROOT} ABSOLUTE)
+  set(CEREAL_ROOT ${_CEREAL_ROOT_ABSOLUTE} CACHE PATH "cereal install path.")
+endif()
+
+#===---------------------------------------------------------------------------------------------===
+#   Find cereal headers
+#====--------------------------------------------------------------------------------------------===
+if(NOT(CEREAL_INCLUDE_DIRS))
+  find_path(CEREAL_INCLUDE_DIRS NAMES cereal/cereal.hpp HINTS ${CEREAL_ROOT})
+endif()
+
+#===---------------------------------------------------------------------------------------------===
+#   Report result 
+#====--------------------------------------------------------------------------------------------===
+find_package_handle_standard_args(
+  cereal
+  FAIL_MESSAGE "Could NOT find CEREAL. (Try setting CEREAL_ROOT in the env)"
+  REQUIRED_VARS  
+    CEREAL_ROOT
+    CEREAL_INCLUDE_DIRS
+)
+
+if(NOT(CEREAL_FOUND) AND cereal_FIND_REQUIRED EQUAL 1)
+  message(FATAL_ERROR "Could NOT find CEREAL. (Try setting CEREAL_ROOT in the env)")
+endif()
+
