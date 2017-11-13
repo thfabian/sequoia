ce85c3adf1a37d0ccbb2f317453a115466ce6579
Fabian <fabian_thuering@hotmail.com>
2017-11-13 22:58:12 +0100

    Fix building assimp on Windows
    - Remove assimps FindZLIB module to use the builtin one

--- cmake/External_assimp.cmake
+++ cmake/External_assimp.cmake
@@ -13,6 +13,9 @@
 ##
 ##===------------------------------------------------------------------------------------------===##
 
+# Pass location of zlib if we build it
+sequoia_extract_cmake_package_args(zlib cmake_args)
+
 ExternalProject_Add(
   assimp
   DOWNLOAD_DIR "${SEQUOIA_EXTERNAL_DOWNLOAD_DIR}/assimp"
@@ -20,13 +23,17 @@ ExternalProject_Add(
   URL_MD5 ${assimp_md5}
   BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}/assimp"
   INSTALL_DIR "${SEQUOIA_EXTERNAL_INSTALL_PREFIX}/assimp"
+  # Remove the custom FinZLIB to force using the CMake builtin one 
+  PATCH_COMMAND ${CMAKE_COMMAND} -E remove -f "<SOURCE_DIR>/cmake-modules/FindZLIB.cmake"
   CMAKE_ARGS
+    ${cmake_args}
     ${SEQUOIA_EXTERNAL_CMAKE_ARGS}
     ${SEQUOIA_EXTERNAL_PROJECTS_CMAKE_ARGS}
     -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
     -DASSIMP_BUILD_TESTS:BOOL=OFF
     -DASSIMP_BUILD_SAMPLES:BOOL=OFF
     -DASSIMP_BUILD_ASSIMP_TOOLS:BOOL=OFF
+    -DASSIMP_INSTALL_PDB:BOOL=OFF
 
     -DASSIMP_BUILD_ALL_IMPORTERS_BY_DEFAULT:BOOL=OFF
     -DASSIMP_BUILD_OBJ_IMPORTER:BOOL=ON
